4fdd4c09ffd19b623d60d86b3682526dae312e7e
Lilith-51 <161232064+Lilith-51@users.noreply.github.com>
2025-12-15 23:31:52 +0700

    Updated at 23:31 12/15/2025

--- Project/Data Structures/Wallet Info/Transaction/Tstn.cpp
+++ Project/Data Structures/Wallet Info/Transaction/Tstn.cpp
@@ -5,49 +5,45 @@
 using namespace std;
 
 bool CheckvalidDate(const Date& c){
-    switch (c.month){
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12: if (c.day>=1&&c.day<=31) return 1;
-                 else return 0;
-        case 4:
-        case 6:
-        case 9:
-        case 11: if (c.day>=1&&c.day<=31) return 1;
-                 else return 0;
-        case 2: if (c.year%400==0||(c.year%4==0&&c.year%100!=0))
-                    if (c.day>=1&&c.day<=29) return 1;
-                    else return 0;
-                else if (c.day>=1&&c.day<=28) return 1;
-                    else return 0;
-        default: return 0;
-    }
+    int time[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+    if (c.year % 4 == 0 && c.year % 100 != 0 || c.year % 400 == 0) time [1] = 29;
+    if (c.day != time[c.month - 1]) return 0;
+    return 1;
 }
-void inputTransactionFromFile(ifstream& fin,Transaction& a){
+
+void inputTransactionFromFile(char* file, Transaction& a){
+    std::ifstream fin;
+    fin.open(file, std::ios::binary);
     fin.read((char*)&a.date,sizeof(Date));
     fin.read((char*)&a.amount, sizeof(a.amount));
     int lengthofstring;
     fin.read((char*)&lengthofstring, sizeof(lengthofstring));
     a.description.resize(lengthofstring);
     fin.read(&a.description[0], lengthofstring);
+    fin.close();
 }
-void outputTransactiontoFile(std::ofstream& fout,const Transaction& a){
+
+void outputTransactiontoFile(char* file,const Transaction& a){
+    std::ofstream fout;
+    fout.open(file, std::ios::binary);
     fout.write((char*)&a.date,sizeof(Date));
     fout.write((char*)&a.amount,sizeof(a.amount));
     int len=a.description.length();
     fout.write((char*)&len, sizeof(len));
     fout.write(&a.description[0],len*sizeof(char));
+    fout.close();
 }
-int CompareDate(const Date& a, const Date& b){
-    if (a.year<b.year) return 1;
-    if (b.year<a.year) return -1;
-    if (a.month<b.month) return 1;
-    if (b.month<a.month) return -1;
-    if (a.day<b.day) return 1;
-    if (b.day<a.day) return -1;
-    return 0;
+
+void genTransaction(Transaction& a) {
+    
+}
+
+/*bool CompareDate(const Date& a, const Date& b){
+    int c = a.year*10000 + a.month*100 + a.day, d = b.year*10000 + b.month*100 + b.day;
+    return (c > d);
+}*/
+
+bool CompareTransaction(const Transaction& a, const Transaction& b) {
+    int c = a.date.year*10000 + a.date.month*100 + a.date.day, d = b.date.year*10000 + b.date.month*100 + b.date.day;
+    return (c > d);
 }
--- Project/Data Structures/Wallet Info/Transaction/Tstn.h
+++ Project/Data Structures/Wallet Info/Transaction/Tstn.h
@@ -13,9 +13,17 @@ struct Transaction{
     long long amount;
     Date date;
     std::string description;
+
+    Transaction();
+    ~Transaction();
 };
 
 bool CheckvalidDate(const Date&); // Only check if it's a valid Date or not - don't care about the current Date
-void inputTransactionFromFile(std::ifstream&,Transaction&); //Date->Amount->size of string ->Note
-void outputTransactiontoFile(std::ofstream&,const Transaction&);
-int CompareTransaction(const Transaction& a, const Transaction& b); //Sort via Date -> Calculate and Statistic
+
+void inputTransactionFromFile(char*, Transaction&); //Date->Amount->size of string ->Note
+
+void outputTransactiontoFile(char*,const Transaction&);
+
+void outputTransactiontoFileFromConsole(char*,const Transaction&);
+
+bool CompareTransaction(const Transaction& a, const Transaction& b); //Sort via Date -> Calculate and Statistic
