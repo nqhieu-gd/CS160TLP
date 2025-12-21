a6194a8ebf1f04767fd69ed0cf870bbc3e7119ff
Lilith-51 <161232064+Lilith-51@users.noreply.github.com>
2025-12-21 23:20:18 +0700

    Small changes

--- Project/Utilities/Uppercase/Uppercase.h
+++ Project/Utilities/Uppercase/Uppercase.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <iostream>
 
-void upper(char &a) {
+inline void upper(char &a) {
     if (a >= 97 && a <= 122) a = a - 32;
 }
 
-void upperStr(std::string &str) {
+inline void upperStr(std::string &str) {
     for (int i = 0; i < str.size(); i++) {
         upper(str[i]);
     }
